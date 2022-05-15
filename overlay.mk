46ec9f5444a758b20c6b71b4d90cf76de54c1b2a
Licht <reftlight309@gmail.com>
2022-05-15 15:49:52 -0500

    initial m52 overlay

--- overlay.mk
+++ overlay.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-m31 \
 	treble-overlay-samsung-m31-systemui \
 	treble-overlay-samsung-m31s \
+	treble-overlay-samsung-m52 \
 	treble-overlay-samsung-n9q \
 	treble-overlay-samsung-o1s \
 	treble-overlay-samsung-p3s \
